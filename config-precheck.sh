5f1ea030ec96c77ddc7b3d26b12269b69d8493ee
architectingit <61459687+architectingit@users.noreply.github.com>
2020-10-26 13:31:27 +0000

    Update config-precheck.sh

--- config-precheck.sh
+++ config-precheck.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-apt -y update
+# Define some colours for later
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33]'
+NC='\033[0m' # No Color
 
-echo "checking installed software and features"
+apt -y update
+apt -y install linux-modules-extra-$(uname -r)
+
+echo -e "${GREEN}Checking installed software and features....${NC}"
 
 dpkg-query --show docker.io &> /dev/null
 
@@ -10,15 +17,18 @@ if [ $? -ne 0 ]
   then
      apt-get -y install docker.io
      systemctl enable docker
-     echo "Installed and started Docker"
+     echo -e "${ORANGE}Installed and started Docker${NC}"
   else 
-     echo "Docker already installed"
+     echo -e "${GREEN}Docker already installed${NC}"
 fi
 dpkg-query --show jq &> /dev/null
 if [ $? -ne 0 ]
   then
      apt -y install jq
-     echo "Installed jq"
+     echo -e "${ORANGE}Installed jq${NC}"
   else
-     echo "jq already installed"
+     echo -e "${GREEN}jq already installed${NC}"
 fi
+echo -e "${GREEN}Configuring StorageOS pre-flight kernel module installations"
+docker run --name enable_lio --privileged --rm --cap-add=SYS_ADMIN -v /lib/modules:/lib/modules -v /sys:/sys:rshared storageos/init:0.2
+echo -e "${GREEN}Pre-testing checks and installations completed${NC}"
