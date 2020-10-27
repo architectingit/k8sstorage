06447229b992c0b57460f6f19861be361a1f392c
architectingit <61459687+architectingit@users.noreply.github.com>
2020-10-27 08:58:52 +0000

    Update install-k8s.sh

--- install-k8s.sh
+++ install-k8s.sh
@@ -1,4 +1,4 @@
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 apt-get -y install kubeadm kubelet kubectl
-swapoff –a
+echo "Manually issue 'sudo swapoff -a'"
