5c409ececf51da470023b92e65671ce1bacfb78e
architectingit <61459687+architectingit@users.noreply.github.com>
2022-03-18 14:24:43 +0000

    Update install-k8s.sh

--- install-k8s.sh
+++ install-k8s.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 apt-get -y install kubeadm=1.22.8-00 kubelet=1.22.8-00 kubectl=1.22.8-00
