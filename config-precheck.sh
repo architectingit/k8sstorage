7a4ae90fb8e1650ad73307635a4d9566d1562da0
architectingit <61459687+architectingit@users.noreply.github.com>
2022-04-08 14:11:54 +0100

    Update config-precheck.sh

--- config-precheck.sh
+++ config-precheck.sh
@@ -7,7 +7,6 @@ ORANGE='\033[0;33]'
 NC='\033[0m' # No Color
 
 apt -y update
-apt -y install linux-modules-extra-$(uname -r)
 
 echo -e "${GREEN}Checking installed software and features....${NC}"
 
@@ -31,7 +30,6 @@ if [ $? -ne 0 ]
 fi
 
 echo -e "${GREEN}Configuring StorageOS pre-flight kernel module installations"
-docker run --name enable_lio --privileged --rm --cap-add=SYS_ADMIN -v /lib/modules:/lib/modules -v /sys:/sys:rshared storageos/init:0.2
 mkdir -p /var/lib/storageos
 echo -e "${GREEN}No specific pre-requisites for Portworx are required${NC}"
 echo -e "${GREEN}Pre-testing checks and installations completed${NC}"
