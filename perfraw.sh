3ee3b058230e3e9a13d5d3c998c35f61f47f2dbe
architectingit <61459687+architectingit@users.noreply.github.com>
2022-05-05 17:00:59 +0100

    Update perfraw.sh

--- perfraw.sh
+++ perfraw.sh
@@ -27,6 +27,7 @@ echo
 
 
     echo Testing Read IOPS...
+    echo 1 > /proc/sys/vm/drop_caches
     READ_IOPS=$(fio --randrepeat=0 --verify=0 --ioengine=libaio --direct=$FIO_DIRECT --gtod_reduce=1 --name=read_iops --filename=$DBENCH_MOUNTPOINT/fiotest --bs=4K --iodepth=16 --fdatasync=$FIO_SYNC --size=$FIO_SIZE --readwrite=randread --time_based --ramp_time=10s --runtime=30s)
     echo "$READ_IOPS"
     READ_IOPS_VAL=$(echo "$READ_IOPS"|grep -E 'read ?:'|grep -Eoi 'IOPS=[0-9k.]+'|cut -d'=' -f2)
@@ -34,6 +35,7 @@ echo
     echo
 
     echo Testing Write IOPS...
+    echo 1 > /proc/sys/vm/drop_caches
     WRITE_IOPS=$(fio --randrepeat=0 --verify=0 --ioengine=libaio --direct=$FIO_DIRECT --gtod_reduce=1 --name=write_iops --filename=$DBENCH_MOUNTPOINT/fiotest --bs=4K --iodepth=16 --fdatasync=$FIO_SYNC --size=$FIO_SIZE --readwrite=randwrite --time_based --ramp_time=10s --runtime=30s)
     echo "$WRITE_IOPS"
     WRITE_IOPS_VAL=$(echo "$WRITE_IOPS"|grep -E 'write:'|grep -Eoi 'IOPS=[0-9k.]+'|cut -d'=' -f2)
@@ -41,6 +43,7 @@ echo
     echo
 
     echo Testing Read Bandwidth...
+    echo 1 > /proc/sys/vm/drop_caches
     READ_BW=$(fio --randrepeat=0 --verify=0 --ioengine=libaio --direct=$FIO_DIRECT --gtod_reduce=1 --name=read_bw --filename=$DBENCH_MOUNTPOINT/fiotest --bs=128K --iodepth=16 --fdatasync=$FIO_SYNC --size=$FIO_SIZE --readwrite=randread --time_based --ramp_time=10s --runtime=30s)
     echo "$READ_BW"
     READ_BW_VAL=$(echo "$READ_BW"|grep -E 'read ?:'|grep -Eoi 'BW=[0-9GMKiBs/.]+'|cut -d'=' -f2)
@@ -48,6 +51,7 @@ echo
     echo
 
     echo Testing Write Bandwidth...
+    echo 1 > /proc/sys/vm/drop_caches
     WRITE_BW=$(fio --randrepeat=0 --verify=0 --ioengine=libaio --direct=$FIO_DIRECT --gtod_reduce=1 --name=write_bw --filename=$DBENCH_MOUNTPOINT/fiotest --bs=128K --iodepth=16 --fdatasync=$FIO_SYNC --size=$FIO_SIZE --readwrite=randwrite --time_based --ramp_time=10s --runtime=30s)
     echo "$WRITE_BW"
     WRITE_BW_VAL=$(echo "$WRITE_BW"|grep -E 'write:'|grep -Eoi 'BW=[0-9GMKiBs/.]+'|cut -d'=' -f2)
@@ -56,6 +60,7 @@ echo
 
     if [ "$DBENCH_QUICK" == "" ] || [ "$DBENCH_QUICK" == "no" ]; then
         echo Testing Read Latency...
+        echo 1 > /proc/sys/vm/drop_caches
         READ_LATENCY=$(fio --randrepeat=0 --verify=0 --ioengine=libaio --direct=$FIO_DIRECT --name=read_latency --filename=$DBENCH_MOUNTPOINT/fiotest --bs=4K --iodepth=4 --size=$FIO_SIZE --readwrite=randread --time_based --ramp_time=10s --runtime=30s)
         echo "$READ_LATENCY"
         READ_LATENCY_VAL=$(echo "$READ_LATENCY"|grep ' lat.*avg'|grep -Eoi 'avg=[0-9.]+'|cut -d'=' -f2)
@@ -63,6 +68,7 @@ echo
         echo
 
         echo Testing Write Latency...
+        echo 1 > /proc/sys/vm/drop_caches
         WRITE_LATENCY=$(fio --randrepeat=0 --verify=0 --ioengine=libaio --direct=$FIO_DIRECT --name=write_latency --filename=$DBENCH_MOUNTPOINT/fiotest --bs=4K --iodepth=4 --size=$FIO_SIZE --readwrite=randwrite --time_based --ramp_time=10s --runtime=30s)
         echo "$WRITE_LATENCY"
         WRITE_LATENCY_VAL=$(echo "$WRITE_LATENCY"|grep ' lat.*avg'|grep -Eoi 'avg=[0-9.]+'|cut -d'=' -f2)
@@ -70,6 +76,7 @@ echo
         echo
 
         echo Testing Read Sequential Speed...
+        echo 1 > /proc/sys/vm/drop_caches
         READ_SEQ=$(fio --randrepeat=0 --verify=0 --ioengine=libaio --direct=$FIO_DIRECT --gtod_reduce=1 --name=read_seq --filename=$DBENCH_MOUNTPOINT/fiotest --bs=1M --iodepth=4 --fdatasync=$FIO_SYNC --size=$FIO_SIZE2 --readwrite=read --time_based --ramp_time=10s --runtime=30s --thread --numjobs=4 --offset=0 --offset_increment=$FIO_OFFSET_INCREMENT)
         echo "$READ_SEQ"
         READ_SEQ_VAL=$(echo "$READ_SEQ"|grep -E 'READ:'|grep -Eoi '(aggrb|bw)=[0-9GMKiBs/.]+'|cut -d'=' -f2)
@@ -77,6 +84,7 @@ echo
         echo
 
         echo Testing Write Sequential Speed...
+        echo 1 > /proc/sys/vm/drop_caches
         WRITE_SEQ=$(fio --randrepeat=0 --verify=0 --ioengine=libaio --direct=$FIO_DIRECT --gtod_reduce=1 --name=write_seq --filename=$DBENCH_MOUNTPOINT/fiotest --bs=1M --iodepth=4 --fdatasync=$FIO_SYNC --size=$FIO_SIZE2 --readwrite=write --time_based --ramp_time=10s --runtime=30s --thread --numjobs=4 --offset=0 --offset_increment=$FIO_OFFSET_INCREMENT)
         echo "$WRITE_SEQ"
         WRITE_SEQ_VAL=$(echo "$WRITE_SEQ"|grep -E 'WRITE:'|grep -Eoi '(aggrb|bw)=[0-9GMKiBs/.]+'|cut -d'=' -f2)
@@ -84,6 +92,7 @@ echo
         echo
 
         echo Testing Read/Write Mixed...
+        echo 1 > /proc/sys/vm/drop_caches
         RW_MIX=$(fio --randrepeat=0 --verify=0 --ioengine=libaio --direct=$FIO_DIRECT --gtod_reduce=1 --name=rw_mix --filename=$DBENCH_MOUNTPOINT/fiotest --bs=4k --iodepth=16 --fdatasync=$FIO_SYNC --size=$FIO_SIZE --readwrite=randrw --rwmixread=75 --time_based --ramp_time=10s --runtime=30s)
         echo "$RW_MIX"
         RW_MIX_R_IOPS=$(echo "$RW_MIX"|grep -E 'read ?:'|grep -Eoi 'IOPS=[0-9k.]+'|cut -d'=' -f2)
